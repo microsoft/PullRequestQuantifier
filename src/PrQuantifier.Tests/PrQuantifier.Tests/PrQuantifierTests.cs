0c53a636ac1d1881c6eb1158df9af26a184f450d
Hardik Goel <hardik.iitr@gmail.com>
2020-11-30 07:41:38 -0800

    Add empty core and test projects (#2)
    Added core project with libgit2sharp package for dealing with git operations.

---
+++ src/PrQuantifier.Tests/PrQuantifier.Tests/PrQuantifierTests.cs
@@ -0,0 +1,29 @@
+﻿namespace PrQuantifier.Tests
+***REMOVED***
+    using Xunit;
+
+    public class PrQuantifierTests
+    ***REMOVED***
+        public PrQuantifierTests()
+        ***REMOVED***
+***REMOVED***
+
+        [Fact]
+        public void Quantify_ReturnsCorrectResult()
+        ***REMOVED***
+            // TODO: Implement
+***REMOVED***
+
+        [Fact]
+        public void QuantifyAgainstBranch_ReturnsCorrectResult()
+        ***REMOVED***
+            // TODO: Implement
+***REMOVED***
+
+        [Fact]
+        public void QuantifyCommit_ReturnsCorrectResult()
+        ***REMOVED***
+            // TODO: Implement
+***REMOVED***
+***REMOVED***
+***REMOVED***
--- src/PrQuantifier/PrQuantifier.cs
+++ src/PrQuantifier/PrQuantifier.cs
@@ -2,17 +2,25 @@ namespace PrQuantifier
 ***REMOVED***
     public class PrQuantifier : IPrQuantifier
     ***REMOVED***
+        private readonly QuantifierOptions options;
+
         public PrQuantifier(string optionsYamlFile)
         ***REMOVED***
 ***REMOVED***
 
         public PrQuantifier(QuantifierOptions options)
         ***REMOVED***
+            this.options = options;
 ***REMOVED***
 
+        /// <inheritdoc />
         public QuantifierResult Quantify()
         ***REMOVED***
-            throw new System.NotImplementedException();
+            // get count of current changes
+
+            // return result
+            var quantifierResult = new QuantifierResult();
+            return quantifierResult;
 ***REMOVED***
 
         public QuantifierResult QuantifyAgainstBranch(string baseBranch)
