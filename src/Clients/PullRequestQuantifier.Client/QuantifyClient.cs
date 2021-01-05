0fbdf9ff8997f11544d8f78e68abb9b9e248dc5b
Dragos Daniel Boia <dradboia@microsoft.com>
2021-01-05 11:09:47 -0800

    update build definition (#5)
    * update build config
    
    * update build
    
    * fix the tests
    
    * fix tests
    
    * add coverlet config
    
    * Fix the issue with the percentile
    
    * fix unit tests
    
    * fix the label bug
    
    * fix th tests
    
    * fix the tests
    
    * fix the tests
    
    * update gitignore
    
    * update build definition
    
    * cleanup namespaces
    
    * update build
    
    * update build
    
    * update  build
    
    * update build
    
    * update vsix client
    
    * fix crash when no context path file is not found
    
    * update vsix client
    
    * update client vsix
    
    * update client vsix
    
    * fix VSIX bug
    
    * update readme

--- src/Clients/PullRequestQuantifier.Client/QuantifyClient.cs
+++ src/Clients/PullRequestQuantifier.Client/QuantifyClient.cs
@@ -5,7 +5,9 @@
     using System.Threading.Tasks;
     using global::PullRequestQuantifier.Abstractions.Context;
     using global::PullRequestQuantifier.Abstractions.Core;
+    using global::PullRequestQuantifier.Abstractions.Exceptions;
     using global::PullRequestQuantifier.GitEngine;
+    using YamlDotNet.Core;
 
     /// <inheritdoc />
     public sealed class QuantifyClient : IQuantifyClient
@@ -18,12 +20,7 @@
             string contextFilePath,
             bool printJson)
         {
-            var context = DefaultContext.Value;
-            if (!string.IsNullOrEmpty(contextFilePath))
-            {
-                context = ContextFactory.Load(contextFilePath);
-            }
-
+            var context = LoadContext(contextFilePath);
             prQuantifier = new PullRequestQuantifier(context);
             this.printJson = printJson;
             gitEngine = new GitEngine();
@@ -59,6 +56,37 @@
             return quantifierResult;
         }
 
+        private Context LoadContext(string contextFilePathOrContent)
+        {
+            var context = DefaultContext.Value;
+            if (string.IsNullOrEmpty(contextFilePathOrContent))
+            {
+                return context;
+            }
+
+            // if no valid context will be loaded then load the default one, don't fail
+            try
+            {
+                var ctx = ContextFactory.Load(contextFilePathOrContent);
+                context = ctx;
+            }
+            catch (YamlException)
+            {
+            }
+            catch (ThresholdException ex)
+            {
+                // misconfiguration then print the exception
+                Console.WriteLine(ex);
+            }
+            catch (ArgumentNullException ex)
+            {
+                // misconfiguration then print the exception
+                Console.WriteLine(ex);
+            }
+
+            return context;
+        }
+
         private QuantifierInput GetChanges(string repoPath)
         {
             var quantifierInput = new QuantifierInput();
@@ -79,8 +107,8 @@
             {
                 Console.ForegroundColor = GetColor(quantifierResult.Color);
                 Console.WriteLine(
-                    $"PrQuantified = {quantifierResult.Label}\t" +
-                    $"Diff +{quantifierResult.QuantifiedLinesAdded} -{quantifierResult.QuantifiedLinesDeleted} (Formula = {quantifierResult.Formula})" +
+                    $"PrQuantified = {quantifierResult.Label},\t" +
+                    $"Diff +{quantifierResult.QuantifiedLinesAdded} -{quantifierResult.QuantifiedLinesDeleted} (Formula = {quantifierResult.Formula})," +
                     $"\tTeam percentiles: additions = {quantifierResult.PercentileAddition}%" +
                     $", deletions = {quantifierResult.PercentileDeletion}%.");
                 Console.ResetColor();
---
+++ src/Clients/PullRequestQuantifier.Vsix.Client/SolutionProjects.cs
@@ -0,0 +1,70 @@
+﻿namespace PullRequestQuantifier.Vsix.Client
+{
+    using System.Collections.Generic;
+    using EnvDTE;
+    using EnvDTE80;
+    using Microsoft.VisualStudio.Shell;
+
+    internal static class SolutionProjects
+    {
+        internal static IEnumerable<Project> Projects()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            Projects projects = GetActiveIDE().Solution.Projects;
+            List<Project> list = new List<Project>();
+            var item = projects.GetEnumerator();
+            while (item.MoveNext())
+            {
+                var project = item.Current as Project;
+                if (project == null)
+                {
+                    continue;
+                }
+
+                if (project.Kind == ProjectKinds.vsProjectKindSolutionFolder)
+                {
+                    list.AddRange(GetSolutionFolderProjects(project));
+                }
+                else
+                {
+                    list.Add(project);
+                }
+            }
+
+            return list;
+        }
+
+        private static DTE2 GetActiveIDE()
+        {
+            // Get an instance of currently running Visual Studio IDE.
+            DTE2 dte2 = Package.GetGlobalService(typeof(DTE)) as DTE2;
+            return dte2;
+        }
+
+        private static IEnumerable<Project> GetSolutionFolderProjects(Project solutionFolder)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            List<Project> list = new List<Project>();
+            for (var i = 1; i <= solutionFolder.ProjectItems.Count; i++)
+            {
+                var subProject = solutionFolder.ProjectItems.Item(i).SubProject;
+                if (subProject == null)
+                {
+                    continue;
+                }
+
+                // If this is another solution folder, do a recursive call, otherwise add
+                if (subProject.Kind == ProjectKinds.vsProjectKindSolutionFolder)
+                {
+                    list.AddRange(GetSolutionFolderProjects(subProject));
+                }
+                else
+                {
+                    list.Add(subProject);
+                }
+            }
+
+            return list;
+        }
+    }
+}
