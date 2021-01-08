56cfb7a2f2d2e00b508af530bf54f8baa6b08d76
Hardik Goel <hagoel@microsoft.com>
2021-01-08 09:55:15 -0800

    Add webhook authentication

--- src/Clients/PullRequestQuantifier.GitHub.Client/QuantifierFunction.cs
+++ src/Clients/PullRequestQuantifier.GitHub.Client/QuantifierFunction.cs
@@ -1,6 +1,10 @@
 namespace PullRequestQuantifier.GitHub.Client
 {
+    using System;
+    using System.IO;
     using System.Net.Http;
+    using System.Security.Cryptography;
+    using System.Text;
     using System.Threading.Tasks;
     using GitHubJwt;
     using Microsoft.AspNetCore.Http;
@@ -9,6 +13,7 @@ namespace PullRequestQuantifier.GitHub.Client
     using Microsoft.Azure.WebJobs.Extensions.Http;
     using Microsoft.Extensions.Logging;
     using Microsoft.Extensions.Options;
+    using Newtonsoft.Json.Linq;
     using Octokit;
     using PullRequestQuantifier.GitHub.Client.GitHubClient;
     using PullRequestQuantifier.GitHub.Client.Telemetry;
@@ -33,17 +38,69 @@ namespace PullRequestQuantifier.GitHub.Client
 
         [FunctionName("quantify")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
             HttpRequest req,
             ILogger log)
         {
+            req.Headers.TryGetValue("X-GitHub-Event", out var eventType);
+            req.Headers.TryGetValue("X-GitHub-Delivery", out var deliveryId);
+            req.Headers.TryGetValue("X-Hub-Signature", out var signature);
+
+            string content;
+            using (var reader = new StreamReader(req.Body))
+            {
+                content = await reader.ReadToEndAsync();
+            }
+
+            JObject payload = JObject.Parse(content);
+
+            var action = (string)payload["action"];
+            var dims = new[]
+            {
+                ("eventType", eventType.ToString()),
+                ("action", action),
+                ("deliveryId", deliveryId.ToString())
+            };
+
+            if (!Authenticate(signature, content))
+            {
+                appTelemetry.RecordMetric(
+                    "GitHubWebhook-AuthFailure",
+                    1,
+                    dims);
+                throw new UnauthorizedAccessException("The signature couldn't be authenticated.");
+            }
+
             var gitHubClientFactory = GitHubClientFactory.Create(
-                "org",
+                "microsoft",
                 new Credentials(gitHubJwtFactory.CreateEncodedJwtToken(), AuthenticationType.Bearer),
                 gitHubAppSettings,
                 appTelemetry);
             await gitHubClientFactory.GetGitHubGitClientAsync();
             return new OkObjectResult("Response from function with injected dependencies.");
+        }
+
+        private bool Authenticate(string signature, string content)
+        {
+            var hashValue = ComputeHash(content);
+
+            if (("sha1=" + hashValue).Equals(signature))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private string ComputeHash(string content)
+        {
+            var secretBytes = Encoding.UTF8.GetBytes(gitHubAppSettings.WebhookSecret);
+            var contentBytes = Encoding.UTF8.GetBytes(content);
+
+            using var hmacSha1 = new HMACSHA1(secretBytes);
+            var contentHash = hmacSha1.ComputeHash(contentBytes);
+
+            return BitConverter.ToString(contentHash).Replace("-", string.Empty).ToLower();
         }
     }
 }
