5beca14fae2fff4390b7578554a0dfefa944ad2e
Hardik <hardik.iitr@gmail.com>
2021-01-24 15:19:13 -0800

    Add support of chunk level parsing in diff parser

--- src/PullRequestQuantifier.Abstractions/Git/DiffParser/DiffParser.cs
+++ src/PullRequestQuantifier.Abstractions/Git/DiffParser/DiffParser.cs
@@ -121,6 +121,13 @@
 
         private void Chunk(string line, Match match)
         {
+            // if the file is null at this point, it means that only a chunk was
+            // passed as input to the parser
+            if (file == null)
+            {
+                Restart();
+            }
+
             inDel = oldStart = int.Parse(match.Groups[1].Value);
             oldLines = match.Groups[2].Success ? int.Parse(match.Groups[2].Value) : 0;
             inAdd = newStart = int.Parse(match.Groups[3].Value);
