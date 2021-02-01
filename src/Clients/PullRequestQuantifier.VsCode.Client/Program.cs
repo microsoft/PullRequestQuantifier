b6bed5b82637c07e0501f733553e00803079241d
Hardik Goel <hardik.iitr@gmail.com>
2021-01-31 17:32:59 -0800

    Add packaging of vsix

---
+++ src/Clients/PullRequestQuantifier.VsCode.Client/Program.cs
@@ -0,0 +1,9 @@
+namespace PullRequestQuantifier.VsCode.Client
+{
+    using System.Threading.Tasks;
+
+    public static class Program
+    {
+        public static async Task Main(string[] args) => await Local.Client.Program.Main(args);
+    }
+}
--- src/Clients/PullRequestQuantifier.VsCode.Client/pull-request-quantifier/src/extension.ts
+++ src/Clients/PullRequestQuantifier.VsCode.Client/pull-request-quantifier/src/extension.ts
@@ -5,7 +5,7 @@ var exec = require("child_process").execFile;
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext): void {
 
-	const exePath: string = context.asAbsolutePath("./out/PullRequestQuantifier.Local.Client.exe");
+	const exePath: string = context.asAbsolutePath("./out/PullRequestQuantifier.VsCode.Client.exe");
 	const statusBarItem: vscode.StatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 999);
 	vscode.workspace.onDidSaveTextDocument((e: vscode.TextDocument) => {
 		exec(exePath, ["-gitrepopath", e.fileName], (_err: any, stdout: string, _stderr: any) => {
