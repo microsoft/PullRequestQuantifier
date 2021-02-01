2d9e5b7582dedce491b1dcaad22abc7806a618c6
Hardik Goel <hardik.iitr@gmail.com>
2021-01-31 16:04:16 -0800

    Add vscode extension

---
+++ src/Clients/PullRequestQuantifier.VsCode.Client/pull-request-quantifier/CHANGELOG.md
@@ -0,0 +1,9 @@
+# Change Log
+
+All notable changes to the "pull-request-quantifier" extension will be documented in this file.
+
+Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
+
+## [Unreleased]
+
+- Initial release
---
+++ src/Clients/PullRequestQuantifier.VsCode.Client/pull-request-quantifier/src/extension.ts
@@ -0,0 +1,20 @@
+import * as vscode from "vscode";
+var exec = require("child_process").execFile;
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext): void {
+
+	const exePath: string = context.asAbsolutePath("./out/PullRequestQuantifier.Local.Client.exe");
+	const statusBarItem: vscode.StatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 999);
+	vscode.workspace.onDidSaveTextDocument((e: vscode.TextDocument) => {
+		exec(exePath, ["-gitrepopath", e.fileName], (_err: any, stdout: string, _stderr: any) => {
+			statusBarItem.hide();
+			statusBarItem.text = stdout.trim();
+			statusBarItem.show();
+		});
+	});
+}
+
+// this method is called when your extension is deactivated
+export function deactivate(): void {}
