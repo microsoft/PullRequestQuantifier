fe791423c5c2da93e50ae79dba2dc1a960b0209e
Dragos Boia <dradboia@microsoft.com>
2021-02-06 12:13:46 -0800

    update config

--- src/PullRequestQuantifier.Feedback.Service/Registrar.cs
+++ src/PullRequestQuantifier.Feedback.Service/Registrar.cs
@@ -1,19 +1,36 @@
 namespace PullRequestQuantifier.Feedback.Service
 {
+    using System;
+    using Azure.Identity;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using PullRequestQuantifier.Common.Azure.BlobStorage;
 
     public static class Registrar
     {
+        internal static void AddSettings(this IConfigurationBuilder builder)
+        {
+            builder.AddJsonFile("appsettings.json", false);
+            string endPoint = builder.Build().GetValue<string>("ConfigEndpoint");
+
+            var managedIdentityCredential = new DefaultAzureCredential();
+            builder.AddAzureAppConfiguration(
+                    options =>
+                        options.Connect(
+                                new Uri(endPoint), managedIdentityCredential)
+                            .ConfigureKeyVault(kv => kv.SetCredential(managedIdentityCredential)))
+                .Build();
+        }
+
         internal static IServiceCollection RegisterServices(
             this IServiceCollection serviceCollection,
             IConfiguration configuration)
         {
-            // todo key vault and get secrets from there
+            var pppConfiguration = configuration.GetSection(nameof(AppConfiguration)).Get<AppConfiguration>();
+
             serviceCollection.AddSingleton<IBlobStorage>(p => new BlobStorage(
-                string.Empty,
-                string.Empty,
+                pppConfiguration.BlobStorageAccountName,
+                pppConfiguration.BlobStorageKey,
                 true));
 
             return serviceCollection;
