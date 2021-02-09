b5d21cc4d6defaabbb0987f250dd56c36e58829c
Hardik Goel <hardik.iitr@gmail.com>
2021-02-09 14:13:54 -0800

    Add parsed short output to vscode status bar

--- src/Clients/PullRequestQuantifier.VsCode.Client/pull-request-quantifier/src/extension.ts
+++ src/Clients/PullRequestQuantifier.VsCode.Client/pull-request-quantifier/src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
 import * as fs from "fs";
+import { parse } from "path";
 var exec = require("child_process").execFile;
 
 // this method is called when your extension is activated
@@ -16,11 +17,14 @@ export function activate(context: vscode.ExtensionContext): void {
 	vscode.workspace.onDidSaveTextDocument((e: vscode.TextDocument) => {
 		exec(exePath, ["-gitrepopath", e.fileName], (_err: any, stdout: string, _stderr: any) => {
 			const consoleOutput: string = stdout.trim();
-			let parsedOutput: RegExpExecArray | null = outputRegex.exec(consoleOutput)
+			let parsedOutput: RegExpExecArray | null = outputRegex.exec(consoleOutput);
+			if (parsedOutput) {
+				const shortOutput: string = `${parsedOutput[1]}, +${parsedOutput[2]} -${parsedOutput[3]}, ${parsedOutput[7]}%`;
 
-			statusBarItem.hide();
-			statusBarItem.text = stdout.trim();
-			statusBarItem.show();
+				statusBarItem.hide();
+				statusBarItem.text = shortOutput.trim();
+				statusBarItem.show();
+			}
 		});
 	});
 }
