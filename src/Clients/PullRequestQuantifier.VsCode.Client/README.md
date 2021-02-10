f5752221e6b9db982744586bd03ecee2d8ea2658
Hardik Goel <hardik.iitr@gmail.com>
2021-02-09 20:31:39 -0800

    Use short status in VSCode status bar

---
+++ src/Clients/PullRequestQuantifier.VsCode.Client/README.md
@@ -0,0 +1,20 @@
+# Visual Studio Code client
+
+## Installation
+
+Install the VSCode extension either through the browser or from VSCode itself.
+
+## Usage
+
+Whenever a file is saved in VSCode, if that file is part of a git repo, the PullRequestQuantifier extension will evaluate changes
+and display information in the status bar about how it is quantified.
+
+![](../../../docs/images/client-vscode-status.png)
+
+Click on the info to show a more detailed output.
+
+![](../../../docs/images/client-vscode-moreinfo.png)
+
+## Developing
+
+TODO
--- src/Clients/PullRequestQuantifier.VsCode.Client/pull-request-quantifier/src/extension.ts
+++ src/Clients/PullRequestQuantifier.VsCode.Client/pull-request-quantifier/src/extension.ts
@@ -1,7 +1,9 @@
 import * as vscode from "vscode";
 import * as fs from "fs";
-import { parse } from "path";
 var exec = require("child_process").execFile;
+
+const moreInfoButton: string = "More info";
+const moreInfoUrl: string = "https://github.com/microsoft/PullRequestQuantifier";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,14 +14,27 @@ export function activate(context: vscode.ExtensionContext): void {
 	outputTemplate = outputTemplate.replace(/{{(.*?)}}/g, "(.*)");
 	const outputRegex: RegExp = new RegExp(outputTemplate);
 
+	const showMoreInfoCommand: string = "prquantifier.showMoreInfo";
+	let moreInfoOutput: string = "";
+	vscode.commands.registerCommand(showMoreInfoCommand, () => {
+		vscode.window.showInformationMessage(moreInfoOutput, moreInfoButton, "Close").then((selectedValue) => {
+			if (selectedValue === moreInfoButton) {
+				// open link to GitHub repo for more info
+				vscode.env.openExternal(vscode.Uri.parse(moreInfoUrl));
+			}
+		});
+	});
+
 	const exePath: string = context.asAbsolutePath("./out/PullRequestQuantifier.VsCode.Client.exe");
 	const statusBarItem: vscode.StatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 999);
+	statusBarItem.command = showMoreInfoCommand;
+
 	vscode.workspace.onDidSaveTextDocument((e: vscode.TextDocument) => {
 		exec(exePath, ["-gitrepopath", e.fileName], (_err: any, stdout: string, _stderr: any) => {
-			const consoleOutput: string = stdout.trim();
-			let parsedOutput: RegExpExecArray | null = outputRegex.exec(consoleOutput);
+			moreInfoOutput = stdout.trim();
+			let parsedOutput: RegExpExecArray | null = outputRegex.exec(moreInfoOutput);
 			if (parsedOutput) {
-				const shortOutput: string = `${parsedOutput[1]}, +${parsedOutput[2]} -${parsedOutput[3]}, ${parsedOutput[7]}%`;
+				const shortOutput: string = `$(tag) ${parsedOutput[1]}, +${parsedOutput[2]} -${parsedOutput[3]}, ${parsedOutput[7]}%`;
 
 				statusBarItem.hide();
 				statusBarItem.text = shortOutput.trim();
@@ -29,9 +44,10 @@ export function activate(context: vscode.ExtensionContext): void {
 	});
 }
 
-function escapeRegExp(text: string) {
+function escapeRegExp(text: string): string {
 	return text.replace(/[.*+?^$()|[\]\\]/g, "\\$&"); // $& means the whole matched string
 }
 
 // this method is called when your extension is deactivated
+// tslint:disable-next-line: no-empty
 export function deactivate(): void { }
