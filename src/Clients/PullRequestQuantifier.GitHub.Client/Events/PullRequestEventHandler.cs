1b6cdb24156e4b371ccc03820d7bfc175f8300af
Hardik Goel <hardik.iitr@gmail.com>
2021-02-20 19:19:31 -0800

    GitHub - remove previous labels if different (#130)
    * Remove previous label if different

--- src/Clients/PullRequestQuantifier.GitHub.Client/Events/PullRequestEventHandler.cs
+++ src/Clients/PullRequestQuantifier.GitHub.Client/Events/PullRequestEventHandler.cs
@@ -1,6 +1,7 @@
 namespace PullRequestQuantifier.GitHub.Client.Events
 {
     using System;
+    using System.Collections.Generic;
     using System.Drawing;
     using System.IO;
     using System.Linq;
@@ -86,7 +87,8 @@ namespace PullRequestQuantifier.GitHub.Client.Events
             await ApplyLabelToPullRequest(
                 payload,
                 gitHubClientAdapter,
-                quantifierClientResult);
+                quantifierClientResult,
+                quantifyClient.Context.Thresholds.Select(t => t.Label));
 
             await UpdateCommentOnPullRequest(
                 payload,
@@ -101,8 +103,11 @@ namespace PullRequestQuantifier.GitHub.Client.Events
             QuantifierResult quantifierClientResult)
         {
             // delete existing comments created by us
-            var existingComments = await gitHubClientAdapter.GetIssueCommentsAsync(payload.Repository.Id, payload.PullRequest.Number);
-            var existingCommentsCreatedByUs = existingComments.Where(ec => ec.User.Login.Equals($"{gitHubClientAdapter.GitHubAppSettings.Name}[bot]"));
+            var existingComments =
+                await gitHubClientAdapter.GetIssueCommentsAsync(payload.Repository.Id, payload.PullRequest.Number);
+            var existingCommentsCreatedByUs = existingComments.Where(
+                ec =>
+                    ec.User.Login.Equals($"{gitHubClientAdapter.GitHubAppSettings.Name}[bot]"));
             foreach (var existingComment in existingCommentsCreatedByUs)
             {
                 await gitHubClientAdapter.DeleteIssueCommentAsync(payload.Repository.Id, existingComment.Id);
@@ -117,7 +122,8 @@ namespace PullRequestQuantifier.GitHub.Client.Events
                 payload.PullRequest.HtmlUrl,
                 payload.PullRequest.User.Login,
                 ShouldPostAnonymousFeedbackLink(payload),
-                new MarkdownCommentOptions { CollapsePullRequestQuantifiedSection = false, CollapseChangesSummarySection = true });
+                new MarkdownCommentOptions
+                    { CollapsePullRequestQuantifiedSection = false, CollapseChangesSummarySection = true });
             await gitHubClientAdapter.CreateIssueCommentAsync(
                 payload.Repository.Id,
                 payload.PullRequest.Number,
@@ -133,7 +139,8 @@ namespace PullRequestQuantifier.GitHub.Client.Events
         private async Task ApplyLabelToPullRequest(
             PullRequestEventPayload payload,
             IGitHubClientAdapter gitHubClientAdapter,
-            QuantifierResult quantifierClientResult)
+            QuantifierResult quantifierClientResult,
+            IEnumerable<string> labelOptionsFromContext)
         {
             // create a new label in the repository if doesn't exist
             try
@@ -151,7 +158,27 @@ namespace PullRequestQuantifier.GitHub.Client.Events
                     new NewLabel(quantifierClientResult.Label, ConvertToHex(color)));
             }
 
-            // apply label to pull request
+            // remove any previous labels applied if it's different
+            // labels do not have the property of who applied them
+            // so we use string matching against the label options present in the context
+            // if label strings have changed in context since last PR update, this will break
+            var existingLabels =
+                await gitHubClientAdapter.GetIssueLabelsAsync(payload.Repository.Id, payload.PullRequest.Number);
+            var existingLabelsByUs = existingLabels.Select(el => el.Name).Intersect(labelOptionsFromContext).ToList();
+            foreach (var existingLabel in existingLabelsByUs)
+            {
+                if (existingLabel == quantifierClientResult.Label)
+                {
+                    continue;
+                }
+
+                await gitHubClientAdapter.RemoveLabelFromIssueAsync(
+                    payload.Repository.Id,
+                    payload.PullRequest.Number,
+                    existingLabel);
+            }
+
+            // apply new label to pull request
             await gitHubClientAdapter.ApplyLabelAsync(
                 payload.Repository.Id,
                 payload.PullRequest.Number,
