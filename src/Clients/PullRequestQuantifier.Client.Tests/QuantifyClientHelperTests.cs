80e5bbd67d75ffbda4fa4929986789b439aba703
Dragos Boia <dradboia@microsoft.com>
2021-03-06 13:39:48 -0800

    more coverage updates

---
+++ src/Clients/PullRequestQuantifier.Client.Tests/QuantifyClientHelperTests.cs
@@ -0,0 +1,16 @@
+﻿namespace PullRequestQuantifier.Client.Tests
+{
+    using System;
+    using PullRequestQuantifier.Client.Helpers;
+    using Xunit;
+
+    public sealed class QuantifyClientHelperTests
+    {
+        [Fact]
+        public void GetColorTest()
+        {
+            // act, assert
+            Assert.True(QuantifyClientHelper.GetColor("Red").Equals(ConsoleColor.Red));
+        }
+    }
+}
