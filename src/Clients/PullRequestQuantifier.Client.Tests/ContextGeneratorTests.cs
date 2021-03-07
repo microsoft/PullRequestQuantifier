a2e8d4d43120aee289268e06f1e6bee992750203
Dragos Boia <dradboia@microsoft.com>
2021-03-07 09:23:55 -0800

    More code coverage improvment part2

---
+++ src/Clients/PullRequestQuantifier.Client.Tests/ContextGeneratorTests.cs
@@ -0,0 +1,46 @@
+﻿namespace PullRequestQuantifier.Client.Tests
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using PullRequestQuantifier.Client.ContextGenerator;
+    using PullRequestQuantifier.Common.Tests;
+    using Xunit;
+
+    public sealed class ContextGeneratorTests
+    {
+        [Fact]
+        public async Task CreateTestRepo()
+        {
+            // setup
+            var gitRepoHelpers = new GitRepoTestHelpers();
+            gitRepoHelpers.CreateRepo();
+            var contextGenerator = new ContextGenerator();
+
+            // act
+            var context = await contextGenerator.Create(gitRepoHelpers.RepoPath);
+
+            // assert
+            Assert.True(context.Thresholds.Count() == 5);
+            Assert.True(context.AdditionPercentile.Count == 1);
+            Assert.True(context.DeletionPercentile.Count == 1);
+
+            // cleanup
+            gitRepoHelpers.DeleteRepoDirectory();
+        }
+
+        [Fact]
+        public async Task CreateTestRepoEmpty()
+        {
+            // setup
+            var contextGenerator = new ContextGenerator();
+
+            // act
+            var context = await contextGenerator.Create(string.Empty);
+
+            // assert
+            Assert.True(context.Thresholds.Count() == 5);
+            Assert.True(context.AdditionPercentile.Count == 66);
+            Assert.True(context.DeletionPercentile.Count == 41);
+        }
+    }
+}
